026fccfa0719d288df99bbcbfdbec679f7bf4e84
Afens <sheith.92@gmail.com>
2016-02-05 13:49:44 +0100

    2/5

--- jquery/P03/script.js
+++ jquery/P03/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  var $editOrig=$('<img src="btnEditar.jpg" class="btn btnImg"/>');
-  var $deltOrig=$('<img src="btnBorrar.jpg" class="btn btnImg"/>');
-  
-  
+  var $editOrig = $('<img src="btnEditar.jpg" class="btn btnImg" data-toggle="modal" data-target="#myModal"/>');
+  var $deltOrig = $('<img src="btnBorrar.jpg" class="btn btnImg"/>');
+
+
   $.getJSON(
     'http://localhost:3000/alumnos/',
     function (data) {
@@ -10,22 +10,22 @@ $(document).ready(function () {
 
       for (i = 0; i < data.length; i++) {
         var $tr = $('<tr></tr>');
-        var $datos=$('<td>'+data[i].nombre+'</td><td>'+data[i].edad+'</td><td>'+data[i].telefono+'</td><td>'+data[i].direccion+'</td><td>'+data[i].curso+'</td><td>'+data[i].repetidor+'</td><td><img src="'+data[i].foto+'" class="foto"/></td>');
-        
+        var $datos = $('<td>' + data[i].nombre + '</td><td>' + data[i].edad + '</td><td>' + data[i].telefono + '</td><td>' + data[i].direccion + '</td><td>' + data[i].curso + '</td><td>' + data[i].repetidor + '</td><td><img src="' + data[i].foto + '" class="foto"/></td>');
+
         var $acciones = $('<td></td>');
         var $editar = $editOrig.clone();
         var $borrar = $deltOrig.clone();
-        
-        
-        $borrar.data('alumno',data[i].id);
-        $editar.data('alumno',data[i].id);
-        
+
+
+        $borrar.data('alumno', data[i].id);
+        $editar.data('alumno', data[i].id);
+
         $editar.on('click', function () {
-          
-          });
-        
+          $('#myModalLabel').html("ID: "+$(this).data('alumno'));
+        });
+
         $borrar.on('click', function () {
-          $this=$(this);
+          $this = $(this);
           $.ajax({
             url: "http://localhost:3000/alumnos/" + $this.data('alumno'),
             type: "DELETE",
@@ -33,22 +33,15 @@ $(document).ready(function () {
               $this.closest('tr').remove();
             }
           });
-          
+
         });
-        
+
         $acciones.append($editar);
         $acciones.append($borrar);
-          
+
         $tr.append($datos);
         $tr.append($acciones);
-        
-        
-        /*
-        $nuevo.data('alumno',data[i].id);
-        
 
-        
-        */
         $lista.append($tr);
       }
 
